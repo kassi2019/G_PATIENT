4a8be1b43e3a286cbab23744f0eb0de2382b3491
kassi2019 <kassifabrice2@gmail.com>
2019-11-18 15:48:07 +0000

    maj

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,7 +3,7 @@
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Vuerouter&Eloquent</title>
+    <title>Laravel</title>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
     
